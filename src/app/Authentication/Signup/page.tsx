41c7b90b222ada3857f766a588bf52c51724df11
EhsanNoori <ehsannoori528@gmail.com>
2025-08-23 17:10:05 +0430

    Handle create account action and display user data

--- src/app/Authentication/Signup/page.tsx
+++ src/app/Authentication/Signup/page.tsx
@@ -1,33 +1,73 @@
 "use client";
-
-import React from "react";
+import { useRouter } from "next/navigation";
+import { stringify } from "querystring";
+import React, { useState } from "react";
 
 export default function SignUpPage() {
+
+          const user =  useRouter();
+
+          //take variables
+          const [name, setName] = useState("");
+          const [email, setEmail] = useState("");
+          const [password, setPassword] = useState("");
+          const [error , setError] = useState("");
+
+          //submit by take form information
+           const SubmitHandle = (e: React.FormEvent) => {
+            e.preventDefault();
+            
+           }
+         
+
+
+           //transfer to local Storage
+           const userData = {name , email};
+
+          const handle = () => {
+            if(!name || !email || !password){
+              setError("Please Fill all Fields");
+              return;
+             }
+  
+            localStorage.setItem("user", JSON.stringify(userData));
+            user.push("./profile");
+          }
+
+
   return (
+
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8">
         <h1 className="text-3xl font-bold text-center text-fuchsia-800 mb-6">
           Sign Up
         </h1>
 
-        <form className="flex flex-col gap-4">
+        <form onSubmit={SubmitHandle} className="flex flex-col gap-4">
           <input
             type="text"
+           value={name}
+           onChange={e=> setName(e.target.value)}
             placeholder="Full Name"
             className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-fuchsia-600"
           />
           <input
             type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
             placeholder="Email Address"
             className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-fuchsia-600"
           />
           <input
             type="password"
+            onChange={e => setPassword(e.target.value)}
+            value={password}
             placeholder="Password"
             className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-fuchsia-600"
           />
           <button
-            type="submit"
+          onClick={handle}
+            
             className="bg-fuchsia-700 text-white p-3 rounded-lg font-semibold hover:bg-fuchsia-800 transition"
           >
             Create Account
---
+++ src/app/Authentication/profile/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+
+export default function Profile(){
+
+
+    
+    const [user , setUser] =  useState<{name:string ; email: string} | null> (null);
+
+    useEffect(() => {
+
+      const storedUser  = localStorage.getItem("user")
+      if (storedUser){
+
+        setUser(JSON.parse(storedUser));
+
+      }
+    }, []);
+
+    if(!user){
+        <p>user Not found Please Sign up first</p>
+    }
+
+
+    return(
+
+
+
+        <div className="flex flex-col relative pt-10 items-center w-full min-h-screen bg-cyan-200">
+            <Link className="absolute left-5 text-blue-700" href="/">Go Home</Link>
+            <h1 className="font-bold text-2xl underline">Your Profile</h1>
+            <div className="flex flex-row gap-10 mt-6">
+        <div className="w-30 border">
+            <span className="font-bold">Name</span>
+            <p>{user?.name}</p>
+        </div>
+        <div className="w-60 border">
+            <span className="font-bold">Email</span>
+            <p>{user?.email}</p>
+            </div>
+            </div>
+        </div>
+
+    );
+}
